92834ebf605383cc52dbc8121bdb2839cf986ba4
PHONG <PHONG@PHONGPV>
2016-11-07 16:15:31 +0700

    Add project files.

---
+++ BOOKS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BOOKS.Startup))]
+namespace BOOKS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
